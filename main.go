fdea33cf227d8e1ef1728f6e51df3b30723afb94
Jason Majors <jasonrmajors@gmail.com>
2019-05-05 16:01:46 -0700

    WIP - refactor to handle 2 file uploads via form

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
 
 	"github.com/jasonmajors/media-upload/backblaze"
 )
 
 const maxUploadSize = 2 * 1024 * 1024 // 2mb
+
+type UploadFile struct {
+	Bytes   []byte
+	Handler *multipart.FileHeader
+}
 
 func fileSizeIsOk(w http.ResponseWriter, r *http.Request) bool {
 	r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
@@ -24,14 +30,63 @@ func validateFileType(fileType string) bool {
 	// A case for all valid mimetypes
 	switch fileType {
 	case "image/jpeg", "image/jpg":
-		fallthrough
+		valid = true
+	case "audio/mpeg":
+		valid = true
 	case "image/png":
 		valid = true
-		break
 	default:
 		valid = false
 	}
 	return valid
+}
+
+func getFileFromForm(r *http.Request, key string) (multipart.File, *multipart.FileHeader) {
+	file, handler, err := r.FormFile(key)
+	if err != nil {
+		fmt.Println("Unable to read file", err)
+		panic("Unable to read file")
+	}
+	defer file.Close()
+
+	return file, handler
+}
+
+func getBytes(file multipart.File) []byte {
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return fileBytes
+}
+
+func getFileBytes(r *http.Request, key string) <-chan UploadFile {
+	// need to return the handler...
+	file, handler := getFileFromForm(r, key)
+	bytesOut := make(chan UploadFile)
+
+	defer file.Close()
+
+	go func(handler *multipart.FileHeader) {
+		fmt.Println("gettin dem bytes")
+
+		fileBytes := getBytes(file)
+		fileType := http.DetectContentType(fileBytes)
+		// Check valid mimetype
+		if valid := validateFileType(fileType); valid != true {
+			panic("getFileBytes: Invalid file type")
+		}
+		uploadFile := new(UploadFile)
+		uploadFile.Handler = handler
+		uploadFile.Bytes = fileBytes
+
+		bytesOut <- *uploadFile
+
+		close(bytesOut)
+	}(handler)
+
+	return bytesOut
 }
 
 func Upload(w http.ResponseWriter, r *http.Request) {
@@ -41,27 +96,10 @@ func Upload(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "The file's too big man")
 			return
 		}
-		// Get the file
-		file, handler, err := r.FormFile("uploadFile")
-		if err != nil {
-			fmt.Println("Unable to read file", err)
-			return
-		}
-		defer file.Close()
-		// Read all of the contents of our file into a byte array
-		fileBytes, err := ioutil.ReadAll(file)
-		if err != nil {
-			fmt.Println(err)
-		}
-		// Get filetype
-		fileType := http.DetectContentType(fileBytes)
-		// Check valid mimetype
-		if valid := validateFileType(fileType); valid != true {
-			fmt.Println("Upload: Invalid file type")
-			return
-		}
-		// To the cloud
-		backblaze.Save(w, fileBytes, handler)
+		bytesOut := getFileBytes(r, "image")
+		uploadFile := <-bytesOut
+
+		backblaze.Save(w, uploadFile.Bytes, uploadFile.Handler)
 	} else {
 		fmt.Fprintf(w, "Method not allowed")
 	}
