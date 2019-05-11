c16d267e93ab4c2f60801cc1e75867c91a8e2208
Jason Majors <jasonrmajors@gmail.com>
2019-05-10 20:37:59 -0700

    Test for heroku

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"mime/multipart"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/jasonmajors/media-upload/backblaze"
 	"github.com/joho/godotenv"
@@ -109,12 +110,12 @@ func Upload(w http.ResponseWriter, r *http.Request) {
 		audioFileBytes := <-audioBytes
 
 		payload := []backblaze.UploadFile{imageFileBytes, audioFileBytes}
+		// To the cloud!
 		responses, err := backblaze.Save(payload)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-
 		response := make(map[string]string)
 
 		for _, backblazeResp := range responses {
@@ -141,7 +142,7 @@ func jsonErr(w http.ResponseWriter, message string, status int) {
 	errResp := make(map[string]string)
 	errResp["error"] = message
 	jsonErr, _ := json.Marshal(errResp)
-
+	time.Sleep(time.Second)
 	w.Write(jsonErr)
 }
 
